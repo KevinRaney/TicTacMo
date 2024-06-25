3087bbd4fb64003a39f9dea911f077e1ee9ccef9
Kevin R. Raney <kevin@raney.co>
2024-06-25 19:33:16 -0400

    Initial Commit

---
+++ TicTacMo/TicTacMoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacMoApp.swift
+//  TicTacMo
+//
+//  Created by Kevin R. Raney on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct TicTacMoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
